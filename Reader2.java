4313bc483aebe239e61d65ed82ae830e6ad61d7a
Ikohido <106984610+Ikohido@users.noreply.github.com>
2022-12-07 20:46:32 +0300

    Add files via upload

---
+++ Reader2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader2 {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader bruh = new BufferedReader(new FileReader("C:/Javapars/report-30-11-2022.csv"))) // path to file which we need  to read.
+        {
+
+            int c;
+            while ((c = bruh.read()) != -1) { //initialization and read symbol by symbol all file.
+
+                System.out.print((char) c); // output all file in console.
+            }
+        } catch (IOException problems) { // initialization catch construction and names it "problems".
+            // catch is construction which handle code and check errors.
+
+            System.out.println(problems.getMessage());
+        }
+    }
+}
