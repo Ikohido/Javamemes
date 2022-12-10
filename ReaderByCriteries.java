68d8f616d6ae4449a09361566a265103932b0f91
Ikohido <106984610+Ikohido@users.noreply.github.com>
2022-12-10 21:46:42 +0300

    Add files via upload

---
+++ ReaderByCriteries.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReaderByCriteries {
+public static void main(String[] args) {
+    String csvFile = "C:/Javapars/employees.csv";
+    String line = "";
+    String csvSplitBy = ",";
+    try (BufferedReader bruh = new BufferedReader(new FileReader(csvFile))) {
+        while ((line = bruh.readLine()) != null) {
+            String[] description = line.split(csvSplitBy);
+
+            System.out.println("id= " + description[0] + ", name=" + description[1] + ", surname=" + description[2] + ", city=" + description[3] + ", birthday=" + description[4] + ", salary=" + description[5] + ", profession=" + description[6] + ", fired=" + description[7] );
+        }
+    } catch (IOException exception) {
+        exception.printStackTrace();
+    }
+}
+}
