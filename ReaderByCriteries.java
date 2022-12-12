31719a3a3f5a3ab1981b7ad027663b63bb2092dc
Ikohido <106984610+Ikohido@users.noreply.github.com>
2022-12-12 22:02:02 +0300

    Add files via upload

--- ReaderByCriteries.java
+++ ReaderByCriteries.java
@@ -1,20 +1,37 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ReaderByCriteries {
-public static void main(String[] args) {
-    String csvFile = "C:/Javapars/employees.csv"; // path to file, which we need  to read.
-    String line = "";
-    String csvSplitBy = ",";
-    try (BufferedReader bruh = new BufferedReader(new FileReader(csvFile))) {
-        while ((line = bruh.readLine()) != null) {
-            String[] description = line.split(csvSplitBy);
 
-            System.out.println("id= " + description[0] + ", name=" + description[1] + ", surname=" + description[2] + ", city=" + description[3] + ", birthday=" + description[4] + ", salary=" + description[5] + ", profession=" + description[6] + ", fired=" + description[7] );
+    public static void main(String[] args) {
+        String csvFile = "C:/Javapars/employees.csv";
+        String line = "";
+        String csvSplitBy = ",";
+        String values = (" id, " + "name, " + "surname, " + "city, " + "birthday, " + "salary, " + "profession, "
+                + "fired ");
+ArrayList Values = new ArrayList();
+
+        Values.add(values);
+        System.out.println(Values);
+        Values.clear();
+        try (BufferedReader bruh = new BufferedReader(new FileReader(csvFile))) {
+            while ((line = bruh.readLine()) != null) {
+                String[] description = line.split(csvSplitBy);
+                ArrayList list = new ArrayList();
+                list.add(description[0]);
+                list.add(description[1]);
+                list.add(description[2]);
+                list.add(description[3]);
+                list.add(description[4]);
+                list.add(description[5]);
+                list.add(description[6]);
+                list.add(description[7]);
+                System.out.println(list);
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
-    } catch (IOException exception) {
-        exception.printStackTrace();
     }
-}
 }
